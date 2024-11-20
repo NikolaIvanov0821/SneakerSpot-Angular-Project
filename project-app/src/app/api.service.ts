225da7d4001e5e6547d32f648701d6abb9ffc4f1
NikolaIvanov0821 <ni.ivanov2008@gmail.com>
2024-11-20 22:48:14 +0200

    initial commit

---
+++ project-app/src/app/api.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Product } from "./types/product";
+import { enviroment } from "../enviroments/environment";
+
+
+@Injectable({
+    providedIn: "root"
+})
+export class ApiService {
+    
+    constructor(private http: HttpClient) {}
+
+    getProducts() {
+        const url = enviroment.apiUrl
+
+         return this.http.get(url + "/themes")
+    }
+}
