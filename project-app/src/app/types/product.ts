6eb2aa2795bf072bb2c45febb85f77bc240a5403
NikolaIvanov0821 <ni.ivanov2008@gmail.com>
2024-11-20 21:12:17 +0200

    Creating object of products and making interface fo a product

---
+++ project-app/src/app/types/product.ts
@@ -0,0 +1,12 @@
+export interface Product {
+    "name": string,
+    "brand": string,
+    "gender": string,
+    "sizes": number[],
+    "price": number,
+    "colorway": string,
+    "style": string,
+    "info": string,
+    "images": string[],
+    "likes": string[]
+}
